c2e1a4aae048cce55954ec116ea028ac4768f4a4
Dmitry Chirkin <dmitry.chirkin@gmail.com>
2015-11-16 00:14:03 +0200

    added missing devtools panel

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -2,13 +2,19 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 import store from "store/configureStore";
 import App from "containers/App";
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
 export default class Root extends Component {
 	render() {
 		return (
-			<Provider store={store}>
-				<App />
-			</Provider>
+			<div>
+				<Provider store={store}>
+					<App />
+				</Provider>
+				<DebugPanel  bottom right top>
+          <DevTools store={store} monitor={LogMonitor} />
+        </DebugPanel>
+			</div>
 		);
 	}
 }
