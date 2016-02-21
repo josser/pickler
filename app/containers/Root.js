3ca4688a52d1b0df3d712083f7b98de23d2c6e91
Dmitry Chirkin <dmitry.chirkin@gmail.com>
2016-02-21 11:36:23 +0200

    updated packages

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -11,9 +11,7 @@ export default class Root extends Component {
 	render() {
 		return (
 			<Provider store={store}>
-				<div>
-					{router}
-				</div>
+				{router}				
 			</Provider>
 		);
 	}
